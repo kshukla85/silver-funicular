6b6305c45b414515ba6de42562610c2d96db1c0e
Kunal Shukla <kunal.shukla285@gmail.com>
2026-02-13 07:37:31 -0600

    Initializing gh-pages branch

---
+++ r_1/r_1.r
@@ -0,0 +1,91 @@
+
+library(tidyverse)
+
+library(ggrepel)
+
+penguins
+glimpse(penguins)
+
+View(penguins)
+
+?penguins
+
+ggplot(data = penguins, 
+mapping = aes(x = flipper_length_mm, y = body_mass_g)) + geom_point(mapping = aes(color = species, shape = species)) +
+geom_smooth(method = "lm") +
+  labs(
+    title = "Body Mass and Flipper Length",
+    subtitle = "Dimensions for Adelie, Chinstrap, and Gentoo Penguins",
+    x = "bill_depth_mm", y = "bill_length_mm (g)",
+    color = "Species", shape = "Species"
+  ) +
+  scale_color_colorblind()
+
+
+ggplot(
+  data = penguins,
+  mapping = aes(x = flipper_length_mm, y = body_mass_g, color = island)
+  )  +
+    geom_point() +
+    geom_smooth(se = FALSE)
+
+
+ggplot(
+  data = penguins,
+  mapping = aes(x = flipper_length_mm, y = body_mass_g)
+) +
+  geom_point() +
+  geom_smooth()
+
+
+ggplot() +
+  geom_point(
+    data = penguins, 
+    mapping = aes(x = flipper_length_mm, y = body_mass_g)) +
+      geom_smooth(
+        data = penguins,
+        mapping = aes(x = flipper_length_mm, y = body_mass_g)
+      )
+
+
+ggplot(
+  data = penguins,
+  mapping = aes(x = flipper_length_mm, y = body_mass_g)) +
+    geom_point()
+
+ggplot(data = penguins, mapping = aes(x = flipper_length_mm, y = body_mass_g)) +
+  geom_point()
+
+penguins |>
+  ggplot(aes(x = flipper_length_mm, y = body_mass_g)) + 
+  geom_point()
+
+ggplot(penguins, aes(x = fct_infreq(species))) +
+  geom_bar()
+
+ggplot(penguins, aes(x = body_mass_g)) +
+  geom_histogram(binwidth = 200)
+
+ggplot(penguins, aes(x = body_mass_g)) +
+  geom_density()
+
+ggplot(penguins, aes(y = species)) +
+  geom_bar()
+
+ggplot(penguins, aes(x = species)) +
+  geom_bar(color = "red")
+ggplot(penguins, aes(x = species)) +
+  geom_bar(fill = "red")  
+
+
+ggplot(data = diamonds, mapping = aes(x = carat)) +
+  geom_histogram(binwidth = 1)
+
+ggplot(penguins, aes(body_mass_g, color = species, fill = species)) + 
+  geom_density(alpha = 0.5)
+
+install.packages("rmarkdown")
+
+knitr::spin("r_1.R", knit = FALSE)
+knitr::spin("/Users/kunalshukla/silver-funicular/r_1/r_1.R", knit = FALSE)
+
